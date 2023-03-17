ed360c77c7a7d75335416a693bd6547315200ef1
mahan <mn.nakhei98@gmail.com>
2023-03-17 15:00:10 +0330

    password of arman and moahmmd is added

--- src/main/java/ir/maktab/connection/JdbcConnection.java
+++ src/main/java/ir/maktab/connection/JdbcConnection.java
@@ -6,9 +6,15 @@ import java.sql.SQLException;
 import java.sql.SQLOutput;
 
 public class JdbcConnection {
-    public static final String DB_URL = "jdbc:postgresql://localhost/irmaktab";
-    public static final String USER = "postgres";
-    public static final String PASS = "1374";
+//    public static final String DB_URL = "jdbc:postgresql://localhost/irmaktab";
+//    public static final String USER = "postgres";
+//    public static final String PASS = "1374";
+public static final String DB_URL = "jdbc:postgresql://localhost/irmaktab";
+       public static final String USER = "postgres";
+   public static final String PASS = "mn652183141";
+//    public static final String DB_URL = "jdbc:postgresql://localhost/irmaktab";
+//    public static final String USER = "postgres";
+//    public static final String PASS = "12345";
     public static Connection getConnection() {
         try {
             Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
