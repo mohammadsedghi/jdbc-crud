7ff3d5d7420ed6e28c7b1e409f3a5f9e9c5f204b
Arman <armanzanbili210@gmail.com>
2023-03-17 14:50:07 +0330

    update project

--- src/main/java/ir/maktab/connection/JdbcConnection.java
+++ src/main/java/ir/maktab/connection/JdbcConnection.java
@@ -8,7 +8,7 @@ import java.sql.SQLOutput;
 public class JdbcConnection {
     public static final String DB_URL = "jdbc:postgresql://localhost/irmaktab";
     public static final String USER = "postgres";
-    public static final String PASS = "12345";
+    public static final String PASS = "1374";
     public static Connection getConnection() {
         try {
             Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
